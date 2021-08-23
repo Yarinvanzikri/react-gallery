413f5240cc7c934c8a101617484a56fa2640221d
YarinVanZikri <yarinvanzikri@gmail.com>
2021-08-23 16:40:16 +0300

    Homework Gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [albums, setAlbum] = useState([]);
+    const [images, setImages] = useState([]);
+
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/albums')
+            .then(res => res.json())
+            .then(data => setAlbum(data))
+
+    },[albums]);
+    const chooseAlbum = (e) => {
+        fetch(
+            `https://jsonplaceholder.typicode.com/photos?albumId=${e.target.selectedIndex}`
+        )
+            .then((res) => res.json())
+            .then((data) => setImages(data));
+    };
+
+
   return (
-    <div className="App">
-      <h1>Select an album:</h1>
-    </div>
+      <div className="App">
+          <h1>Select an album:</h1>
+          <select name="albums" id="albums" onChange={chooseAlbum}>
+              <option>Select an album:</option>
+
+              {albums.map((album) => (
+                  <option value={album.title} key={album.id}>
+                      {album.title}
+                  </option>
+              ))}
+
+          </select>
+          <br />
+          <br />
+          {images.map((pic, i) => (
+              <img src={pic.thumbnailUrl} key={i}  alt={pic.title} />
+          ))}
+      </div>
   );
 }
 
 export default App;
+
+
